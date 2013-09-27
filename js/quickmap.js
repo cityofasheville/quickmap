bf976d3a04c0c76d7d448bddb0ee2aabb7999dd4
Dave Michelson <dmichelson@ashevillenc.gov>
2013-09-27 12:04:05 -0400

    data driven map and example

--- js/quickmap.js
+++ js/quickmap.js
@@ -94,9 +94,9 @@ var QuickMap = {
            crossDomain: true,
            success:function(data){
             
-            selectBox += '<label class="text-info" for="mapsearch">Choose the '+QuickMap.dataMapConfig.fields[0].fieldLabel+'</label>';
-            selectBox += '<select id="mapsearch" class="form-control input-sm text-info"  onchange="QuickMap.zoomMap(this.value,16,false)" >';
-            
+            //selectBox += '<label class="text-info" for="mapsearch"><h3>Choose the '+QuickMap.dataMapConfig.fields[0].fieldLabel+'</h3></label>';
+            //selectBox += '<select id="mapsearch" class="form-control input-sm text-info"  onchange="QuickMap.zoomMap(this.value,16,false)" >';
+            selectBox += '<div class="bs-example">'
             for(var dataIdx=0;dataIdx<data.features.length;dataIdx++ ){
               
                xStr=data.features[dataIdx].geometry.x;
@@ -106,21 +106,22 @@ var QuickMap = {
                
                label=data.features[dataIdx].attributes[QuickMap.dataMapConfig.fields[0].fieldName]
                len=label.length;
-               //make Pulldown common mind width;
-               for(i=len;i<100;i++){
-                label+='&nbsp;';
-               }
 
-               selectBox +=  '<option value=\''+ value  + '\' class="input-sm text-info" >'+ label+ '</option>';
+               //make Pulldown common mind width;
+               //for(i=len;i<100;i++){
+               // label+='&nbsp;';
+               //}
+
+               //selectBox +=  '<option value=\''+ value  + '\' class="input-lg" >'+ label+ '</option>';
+               selectBox += '<span><button  class="btn btn-info btn-sm zmlayer" value=\''+ value  + '\'  onclick="QuickMap.zoomMap(this.value,16,false)" >'+label+'</button><span>';
             }
-            selectBox += '</select>';
+            selectBox += '</div>';
              $('#results').append(selectBox);
            },
            error:function(x,t,m){console.log('fail');}
        });
       }
     }
-    //JSON.stringify(somedata)
   },
   setIdentifyConfig:function(val){QuickMap.identifyConfig=val},
   setTotalRecs:function(val){QuickMap.totalRecs=val;},
