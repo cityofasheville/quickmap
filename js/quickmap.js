a7f4491ebb0f89817682827aabcf3d5896558cd8
Dave Michelson <dmichelson@ashevillenc.gov>
2013-09-30 13:42:15 -0400

    data map pulldown

--- js/quickmap.js
+++ js/quickmap.js
@@ -140,15 +140,24 @@ var QuickMap = {
                 if(QuickMap.dataMapConfig.fields[f].type=='display'){
                   for(var v=0;v<QuickMap.dataMapConfig.fields[f].values.length;v++){
                     if(QuickMap.dataMapConfig.fields[f].values[v].value==data.features[dataIdx].attributes[QuickMap.dataMapConfig.fields[f].fieldName]){
-                      selectBox += '    <li class="dd-primary" ><button class="btn btn-primary bnt-dd" value=\''+ value  + '\' '+ ' onclick="QuickMap.zoomMap(this.value,16,false)" >'+label+'</button></li>'
+
+                      selectBox += '    <li class="dd-primary" ><button class="btn btn-primary bnt-dd" value=\''+ value  + '\' '+ ' onclick="QuickMap.zoomMap(this.value,16,false)" >'
+                      if(dataIdx > 0){
+                        selectBox += '   <span class="glyphicon glyphicon-arrow-up"></span>&nbsp;&nbsp;';
+                      }
+                      selectBox += label        
+                      if(dataIdx < parseInt(data.features.length)-1){
+                        selectBox += '&nbsp;&nbsp;<span class="glyphicon glyphicon-arrow-down"></span>'
+                      }
+                      selectBox += '</button></li>'
                     }
 
                   }
                 }
-
               }
             }
-
+            
+            
             selectBox += '  </ul>'
             selectBox += '</div>'
 
